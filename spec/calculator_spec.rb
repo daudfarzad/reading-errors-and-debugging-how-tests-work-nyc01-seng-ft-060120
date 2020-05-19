299af471ec2bcf6b5b39c8ee63986a9d050da8f2
Farzad Daud <daud.farzad1@gmail.com>
2020-05-19 19:18:01 +0000

    Automatically backed up by Learn

--- spec/calculator_spec.rb
+++ spec/calculator_spec.rb
@@ -4,7 +4,7 @@ describe "./calculator.rb" do
 
   it "contains a local variable called first_number that is assigned to a number" do
     first_number = get_variable_from_file('./calculator.rb', "first_number")
-    first_number = 1
+    
     expect(first_number).to be_an(Integer).or be_a(Float)
   end
 
@@ -26,7 +26,7 @@ describe "./calculator.rb" do
   it "contains a local variable called difference that is assigned to the result of subtracting first_number and second_number" do
     first_number = get_variable_from_file('./calculator.rb', "first_number")
     second_number = get_variable_from_file('./calculator.rb', "second_number")
-    difference = get_variable_from_file('./calculator.rb', "difference")
+    difference = get_variable_from_file('./calculator.rb', "sum" )
 
     expect(difference).to eq(first_number-second_number)
   end
